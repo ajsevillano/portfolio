36b425dae4302cea0e2d82e17fa6c8c4c28a9e8f
Sevi <correosevi@gmail.com>
2021-04-25 13:58:09 +0100

    Create index styles

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,17 @@ import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <>
-      <h1 className={styles.h1}>Homepage</h1>
-    </>
+    <div>
+      <section className={styles.portfolio}>
+        <div className={styles.card}>1</div>
+        <div className={styles.card}>2</div>
+        <div className={styles.card}>3</div>
+        <div className={styles.card}>4</div>
+        <div className={styles.card}>5</div>
+        <div className={styles.card}>6</div>
+        <div className={styles.card}>7</div>
+        <div className={styles.card}>8</div>
+      </section>
+    </div>
   );
 }
