571c815aa3f854d1f35a32063085597d48d9377d
Sevi <correosevi@gmail.com>
2021-04-26 20:09:41 +0100

    Create an useEffect to call the GitHub API

--- components/header/Index.js
+++ components/header/Index.js
@@ -1,11 +1,29 @@
 import styles from './Header.module.scss';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Header = () => {
+  const ApiUrl = 'https://api.github.com/users/ajsevillano/repos';
+  const [data, setData] = useState([]);
+  const mapTheData = data.map((proyects) => (
+    <p key={proyects.id}>{proyects.name}</p>
+  ));
+  useEffect(() => {
+    setTimeout(async function fetchData() {
+      const proyects = await axios.get(ApiUrl);
+      setData(proyects.data);
+      return proyects;
+    }, 150);
+  }, []);
+
   return (
-    <div className={styles.header}>
-      <h1 className={styles.h1}>Hi, I'm Antonio</h1>
-      <h2>Front-end web developer</h2>
-    </div>
+    <>
+      <div className={styles.header}>
+        <h1 className={styles.h1}>Hi, I'm Antonio</h1>
+        <h2>Front-end web developer</h2>
+      </div>
+      <div>{mapTheData}</div>
+    </>
   );
 };
 
