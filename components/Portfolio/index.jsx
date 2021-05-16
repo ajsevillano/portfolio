f4ff48f9af7669fff7e666507acbd3230fc550b3
Sevi <correosevi@gmail.com>
2021-05-16 15:32:45 +0100

    Create a portfolio section

--- components/Portfolio/index.jsx
+++ components/Portfolio/index.jsx
@@ -1,38 +1,59 @@
 import styles from './Portfolio.module.scss';
+import Image from 'next/image';
 
 const Portfolio = () => {
   return (
     <div className={styles.wrapper}>
-      <img
-        className={styles.img}
-        src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-        alt=""
-      />
-      <img
-        className={styles.img}
-        src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-        alt=""
-      />
-      <img
-        className={styles.img}
-        src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-        alt=""
-      />
-      <img
-        className={styles.img}
-        src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-        alt=""
-      />
-      <img
-        className={styles.img}
-        src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-        alt=""
-      />
-      <img
-        className={styles.img}
-        src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-        alt=""
-      />
+      <div className={styles.project}>
+        <img
+          className={styles.img}
+          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
+          alt=""
+        />
+        <p className={styles.projectName}>Top videogames</p>
+      </div>
+      <div className={styles.project}>
+        <img
+          className={styles.img}
+          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
+          alt=""
+        />
+        <p className={styles.projectName}>catalogue-management-app</p>
+      </div>
+      <div className={styles.project}>
+        <img
+          className={styles.img}
+          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
+          alt=""
+        />
+        <p className={styles.projectName}>button-component-devchallenges</p>
+      </div>
+      <div className={styles.project}>
+        <Image
+          className={styles.img}
+          src="/password.jpg"
+          alt="Picture of the author"
+          width={358}
+          height={224}
+        />
+        <p className={styles.projectName}>password-generator</p>
+      </div>
+      <div className={styles.project}>
+        <img
+          className={styles.img}
+          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
+          alt=""
+        />
+        <p className={styles.projectName}>animated-portfolio</p>
+      </div>
+      <div className={styles.project}>
+        <img
+          className={styles.img}
+          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
+          alt=""
+        />
+        <p className={styles.projectName}>firebase-authentication</p>
+      </div>
     </div>
   );
 };
