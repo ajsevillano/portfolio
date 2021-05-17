e0246f71a8937829ba182f4832e2f67677cf6897
Sevi <correosevi@gmail.com>
2021-05-17 19:45:28 +0100

    Create a dinamic fetch for the data from github

--- components/Portfolio/index.jsx
+++ components/Portfolio/index.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const Portfolio = () => {
-  const [repoData, setRepoData] = useState('');
+  const [repoData, setRepoData] = useState([]);
 
   useEffect(async () => {
     try {
@@ -16,60 +16,22 @@ const Portfolio = () => {
       // Error Handler
       console.error(err);
     }
-  }, [repoData]);
+  }, []);
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.project}>
-        <img
-          className={styles.img}
-          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-          alt=""
-        />
-        <p className={styles.projectName}>Top videogames</p>
-      </div>
-      <div className={styles.project}>
-        <img
-          className={styles.img}
-          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-          alt=""
-        />
-        <p className={styles.projectName}>catalogue-management-app</p>
-      </div>
-      <div className={styles.project}>
-        <img
-          className={styles.img}
-          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-          alt=""
-        />
-        <p className={styles.projectName}>button-component-devchallenges</p>
-      </div>
-      <div className={styles.project}>
-        <Image
-          className={styles.img}
-          src="/password.jpg"
-          alt="Picture of the author"
-          width={358}
-          height={224}
-        />
-        <p className={styles.projectName}>password-generator</p>
-      </div>
-      <div className={styles.project}>
-        <img
-          className={styles.img}
-          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-          alt=""
-        />
-        <p className={styles.projectName}>animated-portfolio</p>
-      </div>
-      <div className={styles.project}>
-        <img
-          className={styles.img}
-          src="https://dummyimage.com/716x448/2a2b57/ffffff.png&text=Portfolio"
-          alt=""
-        />
-        <p className={styles.projectName}>firebase-authentication</p>
-      </div>
+      {repoData.map((data) => (
+        <div key={data.id} className={styles.project}>
+          <Image
+            className={styles.img}
+            src={`/${data.name}.png`}
+            alt={data.name}
+            width={358}
+            height={224}
+          />
+          <p className={styles.projectName}>{data.name}</p>
+        </div>
+      ))}
     </div>
   );
 };
