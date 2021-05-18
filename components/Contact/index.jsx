da47515a260f582d8327dc3c567f6c9c445bd6ae
Sevi <correosevi@gmail.com>
2021-05-18 19:58:56 +0100

    Create a contact component

---
+++ components/Contact/index.jsx
@@ -0,0 +1,7 @@
+import styles from './Contact.module.scss';
+
+const Contact = () => {
+  return <div>Contact with me</div>;
+};
+
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import styles from '../styles/Home.module.scss';
 //components
 import Header from '../components/header/Index';
 import Portfolio from '../components/Portfolio';
+import Contact from '../components/Contact';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Portfolio />
+      <Contact />
     </>
   );
 }
