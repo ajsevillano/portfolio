28db602270a11f69f568994a95525ca3eb9bdc77
Sevi <correosevi@gmail.com>
2021-05-19 14:56:31 +0100

    Add image and styles on contact

--- components/Contact/index.jsx
+++ components/Contact/index.jsx
@@ -1,9 +1,12 @@
 import styles from './Contact.module.scss';
+import Image from 'next/image';
 
 const Contact = () => {
   return (
     <div className={styles.wrapper}>
-      <div>Contact</div>
+      <div className={styles.imgWrapper}>
+        <Image src="/me.png" alt="Picture of me" width={600} height={450} />
+      </div>
       <h1 className={styles.title}>Please contact with me</h1>
     </div>
   );
