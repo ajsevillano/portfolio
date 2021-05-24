4db2dcc0f7a63fb42f6a1281e5130d2e75e9de01
Sevi <correosevi@gmail.com>
2021-05-24 10:44:32 +0100

    Add a wave animation to about section

--- components/Contact/index.jsx
+++ components/Contact/index.jsx
@@ -3,21 +3,25 @@ import Image from 'next/image';
 
 const Contact = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.imgWrapper}>
-        <Image src="/me.png" alt="Picture of me" width={600} height={450} />
+    <>
+      <div className={styles.wave}></div>
+      <div className={styles.wrapper}>
+        <div className={styles.imgWrapper}>
+          <Image src="/me.png" alt="Picture of me" width={600} height={450} />
+        </div>
+        <div className={styles.textWrapper}>
+          <h1>Something about me</h1>
+          <p>
+            My name is Antonio Sevillano, I'm a self-taught spanish web
+            developer living in the UK for the last 8 years. Despite having some
+            backend coding knowledge from a long time ago, I recently embrace
+            and felt in love with frontend developing and since then, I haven't
+            stopped learning and pulishing my skills (specially with React.js
+            ⚛️).
+          </p>
+        </div>
       </div>
-      <div className={styles.textWrapper}>
-        <h1>Something about me</h1>
-        <p>
-          My name is Antonio Sevillano, I'm a self-taught spanish web developer
-          living in the UK for the last 8 years. Despite having some backend
-          coding knowledge from a long time ago, I recently embrace and felt in
-          love with frontend developing and since then, I haven't stopped
-          learning and pulishing my skills (specially with React.js ⚛️).
-        </p>
-      </div>
-    </div>
+    </>
   );
 };
 
