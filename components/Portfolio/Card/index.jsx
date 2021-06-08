38aa17ffbebea9239d57d795da221ab9576436ce
Sevi <correosevi@gmail.com>
2021-06-08 15:41:48 +0100

    Create an on hover layer to show links

--- components/Portfolio/Card/index.jsx
+++ components/Portfolio/Card/index.jsx
@@ -3,13 +3,16 @@ import styles from './Card.module.scss';
 const Card = ({ name, description, handleImgError }) => {
   return (
     <div className={styles.card}>
-      <img
-        className={styles.img}
-        src={`/${name}.png`}
-        alt={name}
-        onError={handleImgError}
-        alt="foo"
-      />
+      <div className={styles.linksLayer}>
+        <div className={styles.capa}></div>
+        <img
+          className={styles.img}
+          src={`/${name}.png`}
+          alt={name}
+          onError={handleImgError}
+        />
+      </div>
+
       <h3 className={styles.projectName}>{name}</h3>
       <span className={styles.project_description}>{description}</span>
     </div>
