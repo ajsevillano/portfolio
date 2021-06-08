7c822fa53a9c910e6311e04a619323e6542fe302
Sevi <correosevi@gmail.com>
2021-06-08 19:56:28 +0100

    Add buttons on hover project

--- components/Portfolio/Card/index.jsx
+++ components/Portfolio/Card/index.jsx
@@ -1,4 +1,5 @@
 import styles from './Card.module.scss';
+import Button from '../../Button';
 
 const Card = ({ name, description, handleImgError }) => {
   return (
@@ -6,7 +7,12 @@ const Card = ({ name, description, handleImgError }) => {
       <div className={styles.imgContainer}>
         <div className={styles.hoverContainer}>
           <div className={styles.hoverContent}>
-            <a href="http://www.google.es">Hola</a>
+            <Button variant="secundary">
+              <img src="/link.svg" alt="An SVG of an eye" /> See demo
+            </Button>
+            <Button variant="secundary">
+              <img src="/github.svg" alt="An SVG of an eye" /> See code
+            </Button>
           </div>
         </div>
         <img
