6af2dbbbb900253e1138892b7dda034877608378
Sevi <correosevi@gmail.com>
2021-06-09 14:03:04 +0100

    Refactory demos Urls data to a json file and add fetch

--- components/Portfolio/index.jsx
+++ components/Portfolio/index.jsx
@@ -4,26 +4,37 @@ import styles from './Portfolio.module.scss';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { replaceImgWithError, filterRepositories } from './Portfolio.utils';
-//Data
-import { demoUrl } from '../../data';
 
 //Components
 import Card from './Card';
 
 const Portfolio = () => {
   const [repoData, setRepoData] = useState();
+  const [demoWebUrl, setDemoWebUrl] = useState();
 
   const filterDemoUrl = (repoDataId) => {
-    const demoUrlData = demoUrl.filter((data) => data.id === repoDataId);
+    const demoUrlData = demoWebUrl.filter((data) => data.id === repoDataId);
     return demoUrlData.map((data) => data.url);
   };
 
+  //Github repositories
   useEffect(async () => {
     try {
       const resp = await axios.get(
         `https://api.github.com/users/ajsevillano/repos?sort=created&direction=desc&per_page=11`
       );
       setRepoData(filterRepositories(resp.data));
+    } catch (err) {
+      // Error Handler
+      console.error(err);
+    }
+  }, []);
+
+  //Demos urls data file
+  useEffect(async () => {
+    try {
+      const resp = await axios.get('/demourl.json');
+      setDemoWebUrl(resp.data);
     } catch (err) {
       // Error Handler
       console.error(err);
