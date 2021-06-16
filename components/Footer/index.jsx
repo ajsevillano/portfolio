9e4298c8c7d060421390e451d51875f58405743a
Sevi <correosevi@gmail.com>
2021-06-16 14:50:21 +0100

    Add react icons to Footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,12 +1,14 @@
 import Wrapper from '../Layouts/Wrapper';
 import styles from './Footer.module.scss';
+import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <Wrapper background="#292929" justifycontent="space-around">
-      {/* <h1 className={styles.title}>Hola</h1> */}
-      <div className="worktogether">dribb</div>
-      <div className="social">instagram</div>
+      <div className={styles.worktogether}>Contact me</div>
+      <div className={styles.socialIcons}>
+        <FaGithub size="2rem" />
+      </div>
     </Wrapper>
   );
 };
