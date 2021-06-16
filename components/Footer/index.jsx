08a2aa7872fd3a0176ae7e97cf22eb75553e3e73
Sevi <correosevi@gmail.com>
2021-06-16 14:57:02 +0100

    Add structure for social icons

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,13 +1,24 @@
 import Wrapper from '../Layouts/Wrapper';
 import styles from './Footer.module.scss';
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaEnvelope, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <Wrapper background="#292929" justifycontent="space-around">
       <div className={styles.worktogether}>Contact me</div>
       <div className={styles.socialIcons}>
-        <FaGithub size="2rem" />
+        <div className="contactEmail">
+          <FaEnvelope size="2rem" />
+          <p>contact@ajsevillano.com</p>
+        </div>
+        <div className="Github">
+          <FaGithub size="2rem" />
+          <p>My github</p>
+        </div>
+        <div className="twitter">
+          <FaTwitter size="2rem" />
+          <p>On twitter</p>
+        </div>
       </div>
     </Wrapper>
   );
