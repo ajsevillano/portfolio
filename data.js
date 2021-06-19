81a7c4643af72cc0235a63fe1020435083e2686d
Sevi <correosevi@gmail.com>
2021-06-19 20:38:34 +0100

    Add new image field to data

--- data.js
+++ data.js
@@ -1,14 +1,43 @@
 export const customFields = [
-  { id: 362217174, url: 'https://xenodochial-payne-fabb1a.netlify.app/' },
-  { id: 361238021, url: '' },
-  { id: 357897050, url: 'https://silly-leakey-7be228.netlify.app/' },
-  { id: 354868893, url: 'https://gifted-bhaskara-71af9e.netlify.app/' },
-  { id: 351733523, url: 'https://lucid-goldberg-c11d8e.netlify.app/' },
-  { id: 337684354, url: 'https://www.ajsevillano.com/projects/pim/' },
-  { id: 334662367, url: '' },
+  {
+    id: 362217174,
+    url: 'https://xenodochial-payne-fabb1a.netlify.app/',
+    img: '/top-videogames.png',
+  },
+  {
+    id: 361238021,
+    url: 'https://portfolio.ajsevillano.com',
+    img: '/portfolio.png',
+  },
+  {
+    id: 357897050,
+    url: 'https://silly-leakey-7be228.netlify.app/',
+    img: 'button-component.png',
+  },
+  {
+    id: 354868893,
+    url: 'https://gifted-bhaskara-71af9e.netlify.app/',
+    img: 'animated-portfolio.png',
+  },
+  {
+    id: 351733523,
+    url: 'https://lucid-goldberg-c11d8e.netlify.app/',
+    img: 'password-generator.png',
+  },
+  {
+    id: 337684354,
+    url: 'https://www.ajsevillano.com/projects/pim/',
+    img: 'catalogue-management-app.png',
+  },
+  { id: 334662367, url: '', img: 'firebase-authentication.png' },
   {
     id: 322001903,
     url: 'https://www.ajsevillano.com/projects/music-player/',
+    img: 'react-music-player.png',
   },
-  { id: 302134704, url: 'http://api.uniondistribuidora.com/' },
+  {
+    id: 302134704,
+    url: 'http://api.uniondistribuidora.com/',
+    img: 'api.uniondistribuidora.com.png',
+  },
 ];
