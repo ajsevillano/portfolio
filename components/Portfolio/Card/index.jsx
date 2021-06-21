56b88059c497be10e702d1828f8226a54b86da98
Sevi <correosevi@gmail.com>
2021-06-21 20:21:54 +0100

    Fix an error with the customData on Card component

--- components/Portfolio/Card/index.jsx
+++ components/Portfolio/Card/index.jsx
@@ -2,7 +2,8 @@ import styles from './Card.module.scss';
 import Button from '../../Button';
 
 const Card = ({ name, description, handleImgError, url, customFields }) => {
-  const [demoUrl, projectImg] = customFields[0];
+  const demoUrl = customFields.map((data) => data[0]);
+  const projectImg = customFields.map((data) => data[1]);
 
   return (
     <div className={styles.card}>
@@ -11,13 +12,13 @@ const Card = ({ name, description, handleImgError, url, customFields }) => {
           <div className={styles.hoverContent}>
             <a href={demoUrl} target="_blank">
               <Button variant="secundary">
-                <img src="/link.svg" alt="An SVG of an eye" /> See demo
+                <img src="/link.svg" alt="Demo Link" /> See demo
               </Button>
             </a>
 
             <a href={url} target="_blank">
               <Button variant="secundary">
-                <img src="/github.svg" alt="An SVG of an eye" /> See code
+                <img src="/github.svg" alt="Github" /> See code
               </Button>
             </a>
           </div>
