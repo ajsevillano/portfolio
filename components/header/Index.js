3451b5f3c0ec438be3ad46c5ece807790b1cf9ba
Sevi <correosevi@gmail.com>
2021-07-26 19:37:30 +0100

    Add a section container for header to fix responsive design

--- components/header/Index.js
+++ components/header/Index.js
@@ -14,29 +14,31 @@ const Header = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.header}>
-        <div className={styles.imgContainer}>
-          <img className={styles.image} src="./profile.png" alt="Me" />
-          <p className={styles.presentation}>
-            Hello there! I'm Antonio Sevillano
-          </p>
-        </div>
+      <section className={styles.header}>
+        <div className={styles.container}>
+          <div className={styles.imgContainer}>
+            <img className={styles.image} src="./profile.png" alt="Me" />
+            <p className={styles.presentation}>
+              Hello there! I'm Antonio Sevillano
+            </p>
+          </div>
 
-        <h1 className={styles.h1}>Frontend web developer</h1>
-        <h2 className={styles.h2}>
-          A self-taught 🧔🏻 Web Developer, passionate about what I do, and always
-          learning something new. I like to help business to bring ideas to
-          life. In love with ⚛️
-          <span className={styles.span}>React.js</span>
-        </h2>
-        <div className={styles.buttonContainer}>
-          <Button variant="primary">Let's talk!</Button>
-          <Button variant="outline">
-            <IoChevronDownCircleOutline size={30} />
-            <span>See my work</span>
-          </Button>
+          <h1 className={styles.h1}>Frontend web developer</h1>
+          <h2 className={styles.h2}>
+            A self-taught 🧔🏻 Web Developer, passionate about what I do, and
+            always learning something new. I like to help business to bring
+            ideas to life. In love with ⚛️
+            <span className={styles.span}>React.js</span>
+          </h2>
+          <div className={styles.buttonContainer}>
+            <Button variant="primary">Let's talk!</Button>
+            <Button variant="outline">
+              <IoChevronDownCircleOutline size={30} />
+              <span>See my work</span>
+            </Button>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
