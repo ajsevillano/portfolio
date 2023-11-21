dee32adaebb08b64d5f2dca14351017240b699b4
Sevi <correosevi@gmail.com>
2023-11-21 19:04:59 +0000

    Fix | Close #4 | Update date from static to dinamic

--- components/CopyRight/index.tsx
+++ components/CopyRight/index.tsx
@@ -3,10 +3,11 @@ import Wrapper from '../Layouts/Wrapper';
 import styles from './CopyRight.module.scss';
 
 function CopyRight() {
+  const year = new Date().getFullYear();
   return (
     <Wrapper background="#101114" justifycontent="center">
       <p className={styles.copyrightText}>
-        © 2022 Antonio Sevillano - This project is under MIT license
+        © {year} Antonio Sevillano - This project is under MIT license
       </p>
     </Wrapper>
   );
