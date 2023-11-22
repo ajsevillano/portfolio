1c5241d91eaf631ce969e0733a02de1ac282b88b
Sevi <correosevi@gmail.com>
2023-11-22 20:23:33 +0000

    Chore | Update projects URls

--- data.ts
+++ data.ts
@@ -38,7 +38,7 @@ const customFields: CustomDataArrayTypes[] = [
   },
   {
     id: 362217174,
-    URLDemo: 'https://xenodochial-payne-fabb1a.netlify.app/',
+    URLDemo: 'https://top-videogames-portal.netlify.app/',
     imgDemo: '/top-videogames.png',
   },
   {
@@ -48,28 +48,28 @@ const customFields: CustomDataArrayTypes[] = [
   },
   {
     id: 357897050,
-    URLDemo: 'https://silly-leakey-7be228.netlify.app/',
+    URLDemo: 'https://button-component-display.netlify.app/',
     imgDemo: '/button-component.png',
   },
   {
     id: 354868893,
-    URLDemo: 'https://gifted-bhaskara-71af9e.netlify.app/',
+    URLDemo: 'https://animated-photographer-portfolio.netlify.app/',
     imgDemo: '/animated-portfolio.png',
   },
   {
     id: 351733523,
-    URLDemo: 'https://lucid-goldberg-c11d8e.netlify.app/',
+    URLDemo: 'https://ajs-password-generator.netlify.app/',
     imgDemo: '/password-generator.png',
   },
   {
     id: 337684354,
-    URLDemo: 'https://lucid-agnesi-b35889.netlify.app/',
+    URLDemo: 'https://catalogue-management-portal.netlify.app/',
     imgDemo: '/catalogue-management-app.png',
   },
   { id: 334662367, URLDemo: '', imgDemo: '/firebase-authentication.png' },
   {
     id: 322001903,
-    URLDemo: 'https://elastic-ramanujan-ebba21.netlify.app/',
+    URLDemo: 'https://ajs-react-music-player.netlify.app/',
     imgDemo: '/react-music-player.png',
   },
   {
