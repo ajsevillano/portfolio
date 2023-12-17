2c564eb7720dcedbd33bba50a07d84d6bb8d6353
Sevi <correosevi@gmail.com>
2023-12-17 15:08:56 +0000

    Feat | Create form basic design

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,4 +1,7 @@
 import React, { forwardRef } from 'react';
+import Image from 'next/image';
+import { FaPaperPlane } from 'react-icons/fa';
+import Button from '@components/Button';
 import styles from './Modal.module.scss';
 
 interface Props {
@@ -7,14 +10,51 @@ interface Props {
 
 const Modal = forwardRef<HTMLDialogElement, Props>(({ closeDialog }, ref) => (
   <dialog ref={ref} className={styles.modal}>
+    <button type="button" className={styles.closeButton} onClick={closeDialog}>
+      X
+    </button>
     <div className={styles.content}>
-      <div className={styles.image} />
+      <div className={styles.image}>
+        <Image src="/me.png" alt="Picture of me" width={650} height={500} />
+      </div>
       <div className={styles.textContainer}>
-        <h1>Contact me!</h1>
-        <p>Please contact me for any enquire</p>
-        <button type="button" onClick={closeDialog}>
-          Close
-        </button>
+        <h1 className={styles.modalContentTitle}>
+          🧔🏻 I&apos;d Love to Hear From You!
+        </h1>
+        <p className={styles.subtitle}>
+          Please fill out the form below and I&apos;ll get back to you as soon
+          as possible.
+        </p>
+        <div className={styles.formContainer}>
+          <input
+            className={styles.input}
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Your name"
+          />
+          <input
+            className={styles.input}
+            type="text"
+            name="email"
+            id="email"
+            placeholder="email@company.com"
+          />
+          <textarea
+            className={styles.input}
+            style={{ resize: 'none', overflow: 'hidden', height: '70px' }}
+            name="subject"
+            id="subject"
+            placeholder="Your message"
+          />
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button
+            text="Send form"
+            onclick={closeDialog}
+            icon={<FaPaperPlane />}
+          />
+        </div>
       </div>
     </div>
   </dialog>
