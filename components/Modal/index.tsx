5906ef4e7fca31d65d3c002a34e0a8e4d2c8f407
Sevi <correosevi@gmail.com>
2023-12-26 19:53:34 +0000

    Fix | when ESC modal, button onClick stopped working

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useEffect } from 'react';
 import ContactFormModalContent from '@components/ContactFormModalContent';
 
 import styles from './Modal.module.scss';
@@ -7,13 +7,33 @@ interface Props {
   closeDialog: () => void;
 }
 
-const Modal = forwardRef<HTMLDialogElement, Props>(({ closeDialog }, ref) => (
-  <dialog ref={ref} className={styles.modal}>
-    <button type="button" className={styles.closeButton} onClick={closeDialog}>
-      X
-    </button>
-    <ContactFormModalContent closeDialog={closeDialog} />
-  </dialog>
-));
+const Modal = forwardRef<HTMLDialogElement, Props>(({ closeDialog }, ref) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        closeDialog();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeDialog]);
+
+  return (
+    <dialog ref={ref} className={styles.modal}>
+      <button
+        type="button"
+        className={styles.closeButton}
+        onClick={closeDialog}
+      >
+        X
+      </button>
+      <ContactFormModalContent closeDialog={closeDialog} />
+    </dialog>
+  );
+});
 
 export default Modal;
