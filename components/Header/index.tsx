1cdd2340d4139ab4d4ab5fa26c22f5c9b8478d46
Antonio Sevillano <35935634+ajsevillano@users.noreply.github.com>
2026-06-12 09:35:05 +0100

    Add Open Graph / Twitter meta tags and fix title consistency (#29)

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,22 +6,61 @@ import { FaLinkedin } from 'react-icons/fa';
 import styles from './Header.module.scss';
 import Button from '../Button';
 
+const SITE_URL = 'https://www.ajsevillano.com';
+const SITE_TITLE = 'Antonio Sevillano - Software Engineer';
+const SITE_DESCRIPTION =
+  'Portfolio of Antonio Sevillano, a self-taught software engineer based in the UK building web apps with React, TypeScript, Next.js and Node.js.';
+const OG_IMAGE = `${SITE_URL}/me.png`;
+
+const personJsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'Person',
+  name: 'Antonio Sevillano',
+  alternateName: 'Sevi',
+  url: SITE_URL,
+  image: OG_IMAGE,
+  jobTitle: 'Software Engineer',
+  sameAs: [
+    'https://www.linkedin.com/in/ajsevillano/',
+    'https://github.com/ajsevillano',
+    'https://bsky.app/profile/ajsevillano.bsky.social',
+  ],
+};
+
 function Header() {
   const { openDialog, closeDialog, modalRef } = useContext(ModalContext);
 
   return (
     <>
       <Head>
-        <title>Antonio Sevillano - Software developer</title>
-        <meta
-          name="description"
-          content="Antonio Sevillano - Fullstack developer"
-        />
+        <title>{SITE_TITLE}</title>
+        <meta name="description" content={SITE_DESCRIPTION} />
+        <link rel="canonical" href={`${SITE_URL}/`} />
+
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Antonio Sevillano" />
+        <meta property="og:title" content={SITE_TITLE} />
+        <meta property="og:description" content={SITE_DESCRIPTION} />
+        <meta property="og:url" content={`${SITE_URL}/`} />
+        <meta property="og:image" content={OG_IMAGE} />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={SITE_TITLE} />
+        <meta name="twitter:description" content={SITE_DESCRIPTION} />
+        <meta name="twitter:image" content={OG_IMAGE} />
+
         <meta
           name="google-site-verification"
           content="bc-C1EEIl4rOH0VMnH1Fj7p61l9sMLPXEuD0Q3bHsv0"
         />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          type="application/ld+json"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(personJsonLd) }}
+        />
       </Head>
       <header className={styles.header}>
         <Modal closeDialog={closeDialog} ref={modalRef} />
